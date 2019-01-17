e39751cb40d75738de5d369b895476b6fcc6b340
himanshupiplani <himanshu.piplani@mind-infotech.com>
2019-01-17 16:50:30 +0530

    check box

---
+++ app/src/main/java/com/zxc/roomkotlin/GCRadioButton.kt
@@ -0,0 +1,16 @@
+package com.zxc.roomkotlin
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RadioButton
+
+class GCRadioButton : RadioButton {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun toggle() {
+        isChecked = !isChecked
+    }
+}
